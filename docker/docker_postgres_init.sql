7052252851702367f20ff037f75d5297421f1c2c
malek <malek.benyakhlef@esprit.tn>
2021-06-01 09:04:32 +0100

    update database

---
+++ docker/docker_postgres_init.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE $WORKFLOW_DB_NAME
+    WITH 
+    OWNER = $POSTGRES_USER
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'en_US.utf8'
+    LC_CTYPE = 'en_US.utf8'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
+
+CREATE DATABASE $OBJECTIVE_DB_NAME
+    WITH 
+    OWNER = $POSTGRES_USER
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'en_US.utf8'
+    LC_CTYPE = 'en_US.utf8'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
