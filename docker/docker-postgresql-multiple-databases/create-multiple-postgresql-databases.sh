96ff3306266cb117c80541e32aefb1cbdae598ca
malek <malek.benyakhlef@esprit.tn>
2021-06-01 09:38:08 +0100

    update database script

--- docker/docker-postgresql-multiple-databases/create-multiple-postgresql-databases.sh
+++ docker/docker-postgresql-multiple-databases/create-multiple-postgresql-databases.sh
@@ -7,9 +7,9 @@ function create_user_and_database() {
 	local database=$1
 	echo "  Creating user and database '$database'"
 	psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-	    CREATE USER $database;
+	    CREATE USER $POSTGRES_USER;
 	    CREATE DATABASE $database;
-	    GRANT ALL PRIVILEGES ON DATABASE $database TO $database;
+	    GRANT ALL PRIVILEGES ON DATABASE $database TO $POSTGRES_USER;
 EOSQL
 }
 
